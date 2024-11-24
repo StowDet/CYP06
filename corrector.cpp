6acc5b69da55bc02d3b3651fc2f7e762dd98679e
StowDet <stowdet@gmail.com>
2024-11-24 17:24:03 -0600

    Cuerpo de las funciones declaradas en el .h

---
+++ corrector.cpp
@@ -0,0 +1,14 @@
+#include "corrector.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos) {
+
+}
+void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
+
+}
+void ListaCandidatas(char szPalabrasSugeridas[][TAMTOKEN], int iNumSugeridas, char szPalabras[][TAMTOKEN], int iEstadisticas[], int iNumElementos, char szListaFinal[][TAMTOKEN], int iPeso[], int iNumLista) {
+
+}
