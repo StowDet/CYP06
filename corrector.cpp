b935a7f68c948f277617ce9ab9731316f64d811f
StowDet <stowdet@gmail.com>
2024-11-24 17:37:44 -0600

    Función diccionario.v1

--- corrector.cpp
+++ corrector.cpp
@@ -3,8 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos) {
+void Diccionario(char* szNombre) {
+    FILE* fp = fopen(szNombre, "r"); 
+    if (fp == NULL) {
+        printf("No se pudo abrir el archivo\n");
+        return;
+    }
 
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        printf("%s", buffer); 
+    }
+
+    fclose(fp);
 }
 void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
 
