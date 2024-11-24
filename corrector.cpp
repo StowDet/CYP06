135daebfcad0e7c81fd7f3a88c1616a7e52e6ea5
StowDet <stowdet@gmail.com>
2024-11-24 17:49:45 -0600

    Función Diccionariov.3

--- corrector.cpp
+++ corrector.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define PALABRAS 50000
+
 void Diccionario(char* szNombre) {
     FILE* fp;
     fopen_s(&fp, szNombre, "r");
@@ -11,28 +13,47 @@ void Diccionario(char* szNombre) {
         return;
     }
 
+    char szPalabras[PALABRAS][TAMTOKEN] = { {0} };
+    int iEstadisticas[PALABRAS] = { 0 };
+    int iNumElementos = 0;
+
     char buffer[1024];
     while (fgets(buffer, sizeof(buffer), fp)) {
         int i = 0;
         while (buffer[i] != '\0') {
-            char palabra[50] = { 0 };
+            char palabra[TAMTOKEN] = { 0 };
             int j = 0;
 
-            
             while (buffer[i] != '\0' && buffer[i] != ' ' && buffer[i] != '\n') {
-                palabra[j++] = buffer[i++];
+                palabra[j++] = tolower(buffer[i++]);
             }
 
             if (j > 0) {
                 palabra[j] = '\0';
-                printf("Palabra encontrada: %s\n", palabra);
-            }
+                int encontrada = -1;
 
+                for (int k = 0; k < iNumElementos; k++) {
+                    if (strcmp(szPalabras[k], palabra) == 0) {
+                        encontrada = k;
+                        break;
+                    }
+                }
+
+                if (encontrada != -1) {
+                    iEstadisticas[encontrada]++;
+                }
+                else if (iNumElementos < PALABRAS) {
+                    strcpy(szPalabras[iNumElementos], palabra); 
+                    iEstadisticas[iNumElementos] = 1;
+                    iNumElementos++;
+                }
+            }
             i++;
         }
     }
 
     fclose(fp);
+    printf("Diccionario procesado\n");
 }
 void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
 
