26f3a079891a26777b672744b84be947416b644d
StowDet <stowdet@gmail.com>
2024-11-24 18:48:37 -0600

    Corrector.h

--- corrector.cpp
+++ corrector.cpp
@@ -1,14 +1,20 @@
-#include "corrector.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos) {
-
-}
-void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
-
-}
-void ListaCandidatas(char szPalabrasSugeridas[][TAMTOKEN], int iNumSugeridas, char szPalabras[][TAMTOKEN], int iEstadisticas[], int iNumElementos, char szListaFinal[][TAMTOKEN], int iPeso[], int iNumLista) {
-
-}
+#pragma once
+#define TAMTOKEN 50
+void Diccionario(
+	char* szNombre, //Nombre del archivo, desde donde se lee el diccionario
+	char szPalabras[][TAMTOKEN], //Lista de palabras del diccionario
+	int iEstadisticas[], //Numero de veces que aparece la palabra en el archivo
+	int& iNumElementos); //Numero de elementos
+void ClonaPalabras(
+	char* szPalabraLeida, // Palabra a clonar
+	char szPalabrasSugeridas[][TAMTOKEN], //Lista final de palabras clonadas
+	int& iNumSugeridas); //Numero de elementos en la lista
+void ListaCandidatas(
+	char szPalabrasSugeridas[][TAMTOKEN], //Lista de palabras clonadas
+	int iNumSugeridas, //Lista de palabras clonadas
+	char szPalabras[][TAMTOKEN], //Lista de palabras del diccionario
+	int iEstadisticas[], //Lista de las frecuencias de las palabras
+	int iNumElementos, //Numero de elementos en el diccionario
+	char szListaFinal[][TAMTOKEN], //Lista final de palabras a sugerir
+	int iPeso[], //Peso de las palabras en la lista final
+	int iNumLista); //Numero de elementos en la szListaFinal
