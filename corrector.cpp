33902fff74c13942a296b6f93fd31b364ac08999
StowDet <stowdet@gmail.com>
2024-11-24 18:11:29 -0600

    Función Diccionario, Palabras validas

--- corrector.cpp
+++ corrector.cpp
@@ -5,60 +5,69 @@
 #include <ctype.h>
 #define PALABRAS 50000
 
-void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos) {
+int es_caracter_valido(char c);
+int es_caracter_valido(char c) {
+    char alfabeto[] = "abcdefghijklmnopqrstuvwxyzñáéíóú";
+    c = tolower(c);
+    for (size_t i = 0; i < strlen(alfabeto); i++) {
+        if (c == alfabeto[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos)
+{
     FILE* fp;
     fopen_s(&fp, szNombre, "r");
     if (fp == NULL) {
         printf("No se pudo abrir el archivo: %s\n", szNombre);
         return;
     }
-
     char buffer[1024];
     iNumElementos = 0;
-
     while (fgets(buffer, sizeof(buffer), fp)) {
         int i = 0;
         while (buffer[i] != '\0') {
             char palabra[TAMTOKEN] = { 0 };
             int j = 0;
-
-            
-            while (buffer[i] != '\0' && !strchr(" \t,;().\r\n", buffer[i])) {
+            while (buffer[i] != '\0' && es_caracter_valido(buffer[i])) {
                 palabra[j++] = tolower(buffer[i++]);
             }
-
             if (j > 0) {
                 palabra[j] = '\0';
-
-               
                 int encontrada = -1;
                 for (int k = 0; k < iNumElementos; k++) {
-                    if (strcmp(szPalabras[k], palabra) == 0) {
+                    if (strcmp(szPalabras[k], palabra) == 0 && encontrada != k) {
                         encontrada = k;
-                        break;
                     }
                 }
 
                 if (encontrada != -1) {
-                    
                     iEstadisticas[encontrada]++;
                 }
-                else if (iNumElementos < PALABRAS) {
-                    
-                    strcpy_s(szPalabras[iNumElementos], palabra);
-                    iEstadisticas[iNumElementos] = 1;
-                    iNumElementos++;
+                else {
+
+                    if (iNumElementos < PALABRAS) {
+                        strcpy_s(szPalabras[iNumElementos], palabra);
+                        iEstadisticas[iNumElementos] = 1;
+                        iNumElementos++;
+                    }
+                    else {
+                        fclose(fp);
+                        return;
+                    }
                 }
             }
-
-           
-            while (buffer[i] != '\0' && strchr(" \t,;().\r\n", buffer[i])) {
+            while (buffer[i] != '\0' && !es_caracter_valido(buffer[i])) {
                 i++;
             }
         }
     }
 
     fclose(fp);
+}
 
 void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
 
